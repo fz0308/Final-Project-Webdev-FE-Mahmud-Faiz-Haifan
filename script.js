61b383241e040ffda102b322ef151102a4bbb3dd
Lordsans-404 <70736442+Lordsans-404@users.noreply.github.com>
2025-04-23 21:56:10 +0700

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // Fetch quotes from your API
     async function fetchQuotes() {
         try {
-            const response = await fetch('https://server-wcwebdev-2425.vercel.app/');
+            const response = await fetch('https://test001-2425.vercel.app/api/quotes');
             if (!response.ok) {
                 throw new Error('Failed to fetch quotes');
             }
